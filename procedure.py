4e88f767600f37bc5c0d8aed213788e39f2c0b7d
Lucía <lsimoncelli@itba.edu.ar>
2024-06-22 15:04:12 -0300

    fix issue with performer parameter

--- procedure.py
+++ procedure.py
@@ -1,12 +1,11 @@
 from fhir.resources.procedure import Procedure, ProcedurePerformer
 from fhir.resources.codeableconcept import CodeableConcept
 from fhir.resources.coding import Coding
+from fhir.resources.eventdefinition import EventDefinition
 from fhir.resources.reference import Reference
 from base import get_procedure_snomedct_code, get_patient_id_by_name
 
-def create_procedure_resource(procedure_name = None, status = None, patient_name = None, performed_date = None, performer_id = None):
-    
-    procedure = Procedure()
+def create_procedure_resource(procedure_name = None, status = None, patient_name = None, performed_date = None):
     
     # Get procedure and patient codes
     snomed = get_procedure_snomedct_code(procedure_name)
@@ -15,21 +14,20 @@ def create_procedure_resource(procedure_name = None, status = None, patient_name
     if snomed is None:
         raise ValueError(f"Could not find SNOMED CT code for procedure: {procedure_name}")
     
-    # status: done, in progress, unknown,
-    procedure.status = status
-    procedure.occurrenceDateTime = performed_date
-    
-    procedure.code = CodeableConcept(
-        coding = [
-            Coding(
-                system="http://snomed.info/sct",
-                code=snomed["code"],
-                display=snomed["display"]
-            )
-        ])
-    
-    procedure.subject = Reference(reference = f'Patient/{patient_id}')
-    procedure.performer = performer_id
+    procedure = Procedure(
+        status = status,
+        occurrenceDateTime = performed_date,
+        code = CodeableConcept(
+            coding = [
+                Coding(
+                    system="http://snomed.info/sct",  # SNOMED CT system URL
+                    code=snomed["code"],
+                    display=snomed["display"]
+                )
+            ]
+        ),
+        subject = Reference(reference = f'Patient/{patient_id}'),
+        )
     
     return procedure
     
