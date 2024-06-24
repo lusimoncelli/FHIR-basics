44048a6c859805d199fc6f203841b1748098691d
milalangone <mlangone@itba.edu.ar>
2024-06-24 16:58:33 -0300

    Refactor Procedure resource to instance only required fields initially

--- procedure.py
+++ procedure.py
@@ -1,33 +1,39 @@
-from fhir.resources.procedure import Procedure, ProcedurePerformer
+from fhir.resources.procedure import Procedure
 from fhir.resources.codeableconcept import CodeableConcept
 from fhir.resources.coding import Coding
-from fhir.resources.eventdefinition import EventDefinition
 from fhir.resources.reference import Reference
-from base import get_procedure_snomedct_code, get_patient_id_by_name
+from base import get_snomedct_code, get_patient_id_by_name
 
-def create_procedure_resource(procedure_name = None, status = None, patient_name = None, performed_date = None):
+def create_procedure_resource(procedure_name=None, status=None, patient_name=None, performed_date=None):
     
-    # Get procedure and patient codes
-    snomed = get_procedure_snomedct_code(procedure_name)
+    # Obtener ID del paciente
     patient_id = get_patient_id_by_name(patient_name)
     
-    if snomed is None:
-        raise ValueError(f"Could not find SNOMED CT code for procedure: {procedure_name}")
     
     procedure = Procedure(
         status = status,
-        occurrenceDateTime = performed_date,
-        code = CodeableConcept(
-            coding = [
+        subject = Reference(reference=f'Patient/{patient_id}')
+    )
+
+    # Add occurrenceDateTime if available
+    if performed_date:
+        procedure.occurrenceDateTime = performed_date
+
+    # Add code for the procedure
+    if procedure_name:
+        snomed = get_snomedct_code(procedure_name)
+        if snomed is None:
+            raise ValueError(f"Could not find SNOMED CT code for procedure: {procedure_name}")
+        procedure.code = CodeableConcept(
+            coding=[
                 Coding(
                     system="http://snomed.info/sct",  # SNOMED CT system URL
                     code=snomed["code"],
                     display=snomed["display"]
                 )
             ]
-        ),
-        subject = Reference(reference = f'Patient/{patient_id}'),
         )
-    
+
     return procedure
+
     
