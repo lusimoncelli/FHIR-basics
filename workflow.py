f754e76878f6fe54a6f5891fad7e3e042ae189f7
milalangone <mlangone@itba.edu.ar>
2024-06-24 18:11:41 -0300

    Add code for better visualization

--- workflow.py
+++ workflow.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     # Parámetros del paciente (se puede dejar algunos vacíos)
     family_name = "Langone"
     given_name = "Mila"
-    birth_date = "1990-01-01"
+    birth_date = "2000-12-20"
     gender = "female"
     phone = None 
 
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     
     # Parametros de la condicion
-    condition_name = 'Coronary Artery Disease'
+    condition_name = 'Coronary arteiosclerosis'
     clinical_status = "active"
     verification_status = "confirmed"
     onset_date = "2024-05-24"
@@ -36,9 +36,13 @@ if __name__ == "__main__":
     procedure_id = send_resource_to_hapi_fhir(procedure, 'Procedure')
     
     # Ver el recurso de paciente creado
+    print('\n')
     if patient_id:
         get_resource_from_hapi_fhir(patient_id,'Patient')
+    print('\n')
     if procedure_id:
         get_resource_from_hapi_fhir(procedure_id, 'Procedure')
+    print('\n')
     if condition_id:
         get_resource_from_hapi_fhir(condition_id, 'Condition')
+    print('\n')
